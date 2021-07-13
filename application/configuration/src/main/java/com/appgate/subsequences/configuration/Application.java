67c23d47114b0b1da58de9beae2cf2bd30ccbf98
danielbellon <daniel.bellon@payulatam.com>
2021-07-12 20:20:59 -0500

    make project scaffolding

---
+++ application/configuration/src/main/java/com/appgate/subsequences/configuration/Application.java
@@ -0,0 +1,16 @@
+package com.appgate.subsequences.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:danielbellon77@gmail.com"> Daniel Bellón </a>
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
